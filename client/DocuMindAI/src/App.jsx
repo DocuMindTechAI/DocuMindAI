3dc38d6ba1b73e64c471415050dc65e0a6ac8fbc
Heydhal Heydhal <fadhal.shulhan@gmail.com>
2025-05-05 16:48:52 +0700

    feat: setup project for client with react vite tailwind and server with sequelize and express

---
+++ client/DocuMindAI/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 class="text-4xl font-bold ">Hello world!</h1>
+      <button className="px-4 py-2 rounded bg-green-600 hover:bg-green-500 text-white">
+        Tes Tailwind
+      </button>
+    </div>
+  );
+}
+
+export default App;
